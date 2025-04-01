04c458061be603578daa7452ac791396e4cbf286
박기현 <kihyun1998@naver.com>
2025-04-01 17:15:08 +0900

    Update: set mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EventModule } from './event/event.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.MONGO_URI || 'mongodb://localhost:27017/appdb',
     ),
+    EventModule,
   ],
   controllers: [AppController],
   providers: [AppService],
