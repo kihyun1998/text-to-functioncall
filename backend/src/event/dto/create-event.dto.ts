2dc44855d6eccfd00aeae81e6b3adbfdf03f3183
박기현 <kihyun1998@naver.com>
2025-04-01 17:10:49 +0900

    Add: add create event dto

---
+++ backend/src/event/dto/create-event.dto.ts
@@ -0,0 +1,42 @@
+import { IsEnum, IsOptional, IsString, IsDate } from 'class-validator';
+import {
+  EventType,
+  NodePlatform,
+  NodeType,
+  Protocol,
+  Result,
+} from '../event.enum';
+
+export class CreateEventDto {
+  @IsEnum(NodeType)
+  nodeType: NodeType;
+
+  @IsString() nodeId: string;
+  @IsOptional() @IsString() nodeName?: string;
+  @IsOptional() @IsString() nodeAddress?: string;
+  @IsOptional() @IsString() nodeHost?: string;
+
+  @IsOptional() @IsEnum(NodePlatform) nodePlatform?: NodePlatform;
+  @IsOptional() @IsString() nodeOsVersion?: string;
+  @IsOptional() @IsString() nodeAgentVersion?: string;
+  @IsOptional() @IsString() nodeLinkedServer?: string;
+
+  @IsOptional() @IsEnum(EventType) eventType?: EventType;
+  @IsOptional() @IsString() userId?: string;
+  @IsOptional() @IsString() userName?: string;
+  @IsOptional() @IsString() loginAccount?: string;
+  @IsOptional() @IsString() clientIp?: string;
+  @IsOptional() @IsString() sessionId?: string;
+  @IsOptional() @IsString() summary?: string;
+  @IsOptional() @IsString() description?: string;
+  @IsOptional() @IsString() sessionDeviceId?: string;
+  @IsOptional() @IsString() sid?: string;
+  @IsOptional() @IsString() pid?: string;
+
+  @IsOptional() @IsEnum(Protocol) protocol?: Protocol;
+  @IsOptional() @IsEnum(Result) result?: Result;
+
+  @IsOptional() @IsDate() loginAt?: Date;
+  @IsOptional() @IsDate() occurredAt?: Date;
+  @IsOptional() @IsDate() savedAt?: Date;
+}
